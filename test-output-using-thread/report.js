ef1703549af3b56033541f880be9e6c3af9aede5
SaberYahya <s.yahya.h.saber@gmail.com>
2022-08-24 05:10:09 -0400

    I tried to use LocalThread for driver but made a mess LoL

---
+++ test-output-using-thread/report.js
@@ -0,0 +1,153 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "123dd0bc-b5bd-40ed-a6f0-bde4b6b25d4b",
+    "feature": "Laptop \u0026 NoteBooks",
+    "scenario": "Product Comparison",
+    "start": 1661331464964,
+    "group": 1,
+    "content": "",
+    "tags": "@laptopnotebookspage,,@regression,@comparison,",
+    "end": 1661331467263,
+    "className": "failed"
+  },
+  {
+    "id": "f0daffd0-a859-4841-80fe-83e2daa7a9ed",
+    "feature": "Laptop \u0026 NoteBooks",
+    "scenario": "Add and Remove a Mac book from Cart",
+    "start": 1661331454219,
+    "group": 1,
+    "content": "",
+    "tags": "@laptopnotebookspage,,@regression,@addandremovemacbookfromcart,",
+    "end": 1661331464943,
+    "className": "failed"
+  },
+  {
+    "id": "45fbaddc-8868-41c9-a619-813980c98c40",
+    "feature": "Home Page",
+    "scenario": "User empty Shopping cart message displays",
+    "start": 1661331451676,
+    "group": 1,
+    "content": "",
+    "tags": "@homepage,,@regression,@emptycart,",
+    "end": 1661331454185,
+    "className": "failed"
+  },
+  {
+    "id": "2e8035dc-deda-40d9-9ab3-fad4a8c8d906",
+    "feature": "Laptop \u0026 NoteBooks",
+    "scenario": "Adding an item to Wish list",
+    "start": 1661331467285,
+    "group": 1,
+    "content": "",
+    "tags": "@laptopnotebookspage,,@regression,@wishlist,",
+    "end": 1661331470404,
+    "className": "failed"
+  },
+  {
+    "id": "fca9d61c-2a3d-45a2-b63f-4d031b5dcb10",
+    "feature": "Retail Page",
+    "scenario": "Edit your affiliate information from Cheque payment method to Bank Transfer",
+    "start": 1661331490804,
+    "group": 1,
+    "content": "",
+    "tags": "@retailpage,,@regression,@paymentmethodedit,",
+    "end": 1661331493932,
+    "className": "failed"
+  },
+  {
+    "id": "10cb9696-980d-4cd6-9fd5-575f5f19dfe7",
+    "feature": "Desktops Tab",
+    "scenario": "User verify all items are present in Desktops tab",
+    "start": 1661331437599,
+    "group": 1,
+    "content": "",
+    "tags": "@desktoppage,,@regression,@allitemsvisibleindesktoplink,",
+    "end": 1661331442092,
+    "className": "failed"
+  },
+  {
+    "id": "45f184d7-0301-4659-9d30-cff72c95927b",
+    "feature": "Desktops Tab",
+    "scenario": "User add HP LP 3065  from Desktops tab to the cart",
+    "start": 1661331442258,
+    "group": 1,
+    "content": "",
+    "tags": "@desktoppage,,@regression,@addinghptocart,",
+    "end": 1661331443980,
+    "className": "failed"
+  },
+  {
+    "id": "07df333d-cbd5-4611-8f77-34fccbc8ff5e",
+    "feature": "Retail Page",
+    "scenario": "Edit your account Information",
+    "start": 1661331493952,
+    "group": 1,
+    "content": "",
+    "tags": "@retailpage,,@regression,@accountinfoedit,",
+    "end": 1661331497208,
+    "className": "failed"
+  },
+  {
+    "id": "ef069310-5b6d-4a82-9789-d36b2b6720c6",
+    "feature": "Home Page",
+    "scenario": "User verify currency values change",
+    "start": 1661331449159,
+    "group": 1,
+    "content": "",
+    "tags": "@homepage,,@regression,@currency,",
+    "end": 1661331451624,
+    "className": "failed"
+  },
+  {
+    "id": "65b2c989-dbfe-400d-8b28-e45b388569d9",
+    "feature": "Retail Page",
+    "scenario": "Register as an Affiliate user with Cheque Payment Method",
+    "start": 1661331472862,
+    "group": 1,
+    "content": "",
+    "tags": "@retailpage,,@regression,@paymentmethod,",
+    "end": 1661331490775,
+    "className": "failed"
+  },
+  {
+    "id": "f1843d3c-2c22-405f-8c01-31b9cc7f1667",
+    "feature": "Laptop \u0026 NoteBooks",
+    "scenario": "Validate the price of MacBook Pro is 2000",
+    "start": 1661331470449,
+    "group": 1,
+    "content": "",
+    "tags": "@laptopnotebookspage,,@regression,@pricevalidation,",
+    "end": 1661331472832,
+    "className": "failed"
+  },
+  {
+    "id": "976819b9-7a55-4551-99be-108302bb87dd",
+    "feature": "Desktops Tab",
+    "scenario": "User add Canon EOS 5D from Desktops tab to the cart",
+    "start": 1661331444032,
+    "group": 1,
+    "content": "",
+    "tags": "@desktoppage,,@regression,@addingcanontocart,",
+    "end": 1661331446515,
+    "className": "failed"
+  },
+  {
+    "id": "c99990e2-f12c-448a-a711-679c954223bd",
+    "feature": "Desktops Tab",
+    "scenario": "User add a review to Canon EOS 5D item inDesktops tab",
+    "start": 1661331446582,
+    "group": 1,
+    "content": "",
+    "tags": "@desktoppage,,@regression,@reviewpossibility,",
+    "end": 1661331449092,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
